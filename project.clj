6a467b5c36d436443597732488b52206c89caebe
Emil Bengtsson <emil@emil0r.com>
2013-10-01 14:44:23 +0200

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject reverie-template/lein-template "0.1.0-SNAPSHOT"
+  :description "template for setting up a basic reverie installation"
+  :url "http://reveriecms.org/template"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :eval-in-leiningen true)
