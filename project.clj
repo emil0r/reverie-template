22d47f501dd2076ec627a2fb2d3c38b495de200e
Emil Bengtsson <emil@emil0r.com>
2013-10-02 21:10:49 +0200

    changed url for project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject reverie/lein-template "1.0-SNAPSHOT"
   :description "template for setting up a basic reverie installation"
-  :url "http://reveriecms.org/template"
+  :url "http://reveriecms.org/getting-started"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
